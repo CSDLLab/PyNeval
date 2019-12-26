7b2eccaf02bd97120bb0ed97d81db1b7c5b613af
bennieHan <bennieHan98@gmail.com>
2019-12-26 14:28:15 +0800

    update detail output

--- pymets/metric/length_metric.py
+++ pymets/metric/length_metric.py
@@ -7,6 +7,7 @@ from pymets.io.read_json import read_json
 from pymets.io.save_swc import save_as_swc,print_swc
 
 import time
+import os
 
 
 def length_metric_3(gold_swc_tree=None, test_swc_tree=None, DEBUG=False):
@@ -16,15 +17,16 @@ def length_metric_3(gold_swc_tree=None, test_swc_tree=None, DEBUG=False):
     return match_fail
 
 
-def length_metric_2(gold_swc_tree=None, test_swc_tree=None, dis_threshold=0.1, DEBUG=True):
-    match_edges = get_match_edges_e_fast(gold_swc_tree, test_swc_tree,dis_threshold, DEBUG=DEBUG)
+def length_metric_2(gold_swc_tree=None, test_swc_tree=None, dis_threshold=0.1, detail_path=None, DEBUG=True):
+    match_edges = get_match_edges_e_fast(gold_swc_tree, test_swc_tree,  # tree data
+                                         dis_threshold, detail_path, DEBUG=DEBUG)  # configs
 
     match_length = 0.0
     for line_tuple in match_edges:
         match_length += line_tuple[0].parent_distance()
 
-    gold_total_length = round(gold_swc_tree.length(),8)
-    match_length = round(match_length,8)
+    gold_total_length = round(gold_swc_tree.length(), 8)
+    match_length = round(match_length, 8)
 
     if DEBUG:
         print("match_length a = {}, gold_total_length = {}"
@@ -42,7 +44,7 @@ def length_metric_1(gold_swc_tree=None, test_swc_tree=None, DEBUG=False):
     return 1 - test_total_length/gold_total_length
 
 
-def length_metric(gold_swc_tree, test_swc_tree, config):
+def length_metric(gold_swc_tree, test_swc_tree, abs_dir, config):
     if "knn" in config.keys():
         knn = config["knn"]
 
@@ -55,6 +57,8 @@ def length_metric(gold_swc_tree, test_swc_tree, config):
         except:
             raise Exception("[Error: ] Read config info threshold {}. suppose to be a float or \"default\"")
 
+    detail_path = config["detail"]
+    detail_path = os.path.join(abs_dir, detail_path)
     if config["method"] == 1:
         return length_metric_1(gold_swc_tree=gold_swc_tree,
                                test_swc_tree=test_swc_tree)
@@ -62,7 +66,8 @@ def length_metric(gold_swc_tree, test_swc_tree, config):
         print("result = {}".format(length_metric_2(gold_swc_tree=gold_swc_tree,
                                 test_swc_tree=test_swc_tree,
                                 dis_threshold=dis_threshold,
-                                DEBUG=True)))
+                                detail_path=detail_path,
+                                DEBUG=False)))
     elif config["method"] == 3:
         match_fail_tuple_set = length_metric_3(gold_swc_tree=gold_swc_tree,
                                                 test_swc_tree=test_swc_tree,
@@ -78,15 +83,17 @@ def length_metric(gold_swc_tree, test_swc_tree, config):
 
 if __name__ == "__main__":
     goldtree = SwcTree()
-    goldtree.load("D:\gitProject\mine\PyMets\\test\data_example\gold\\sample_rate.swc")
+    goldtree.load("D:\gitProject\mine\PyMets\\test\data_example\gold\\ExampleGoldStandard.swc")
 
     testTree = SwcTree()
-    testTree.load("D:\gitProject\mine\PyMets\\test\data_example\\test\\sample_rate.swc")
+    testTree.load("D:\gitProject\mine\PyMets\\test\data_example\\test\\ExampleTest.swc")
     start = time.time()
-    # length_metric(gold_swc_tree=goldtree,
-    #               test_swc_tree=testTree,
-    #               config=read_json("D:\gitProject\mine\PyMets\config\length_metric.json"))
-    length_metric(gold_swc_tree=testTree,
-                  test_swc_tree=goldtree,
+    length_metric(gold_swc_tree=goldtree,
+                  test_swc_tree=testTree,
+                  abs_dir="D:\gitProject\mine\PyMets",
                   config=read_json("D:\gitProject\mine\PyMets\config\length_metric.json"))
+    # length_metric(gold_swc_tree=testTree,
+    #               test_swc_tree=goldtree,
+    #               abs_dir="D:\gitProject\mine\PyMets",
+    #               config=read_json("D:\gitProject\mine\PyMets\config\length_metric.json"))
     print("time cost = {}".format(time.time() - start))
