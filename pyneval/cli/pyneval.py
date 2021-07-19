5ef2b875e18d92c3c2158386128e627c34b046c0
FeifeiyuM <ayifeiyu@gmail.com>
2021-07-19 17:02:04 +0800

    fix: add file name for console result

--- pyneval/cli/pyneval.py
+++ pyneval/cli/pyneval.py
@@ -179,6 +179,7 @@ def excute_metric(metric, gold_swc_tree, test_swc_tree, config, detail_dir, outp
     )
 
     print ("---------------Result---------------")
+    print ("file = {}".format(test_swc_name))
     for key in result:
         print ("{} = {}".format(key.ljust(15, " "), result[key]))
     print ("----------------End-----------------\n")
